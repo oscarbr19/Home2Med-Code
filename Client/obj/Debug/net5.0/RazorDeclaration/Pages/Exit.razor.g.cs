c762575c45fd5d051d1e8e14168fc62625ea4900
= <=>
2021-10-07 19:02:55 -0600

    commit ensayo

--- Client/obj/Debug/net5.0/RazorDeclaration/Pages/Exit.razor.g.cs
+++ Client/obj/Debug/net5.0/RazorDeclaration/Pages/Exit.razor.g.cs
@@ -13,84 +13,84 @@ namespace Home2Med.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "F:\Home2Med\Client\_Imports.razor"
+#line 1 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "F:\Home2Med\Client\_Imports.razor"
+#line 2 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "F:\Home2Med\Client\_Imports.razor"
+#line 3 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "F:\Home2Med\Client\_Imports.razor"
+#line 4 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "F:\Home2Med\Client\_Imports.razor"
+#line 5 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "F:\Home2Med\Client\_Imports.razor"
+#line 6 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "F:\Home2Med\Client\_Imports.razor"
+#line 7 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "F:\Home2Med\Client\_Imports.razor"
+#line 8 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "F:\Home2Med\Client\_Imports.razor"
+#line 9 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Home2Med.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "F:\Home2Med\Client\_Imports.razor"
+#line 10 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Home2Med.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "F:\Home2Med\Client\_Imports.razor"
+#line 11 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Home2Med.Shared.Entity;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "F:\Home2Med\Client\_Imports.razor"
+#line 12 "F:\Home2Med\Home2Med-Code-1\Client\_Imports.razor"
 using Home2Med.Client.Services;
 
 #line default
